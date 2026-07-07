c5dce166d4cfd4ca6042eab802dcede8712e6998
Алексей Коленко <bekrunaz2293@gmail.com>
2026-07-07 22:30:49 +0300

    исправление ошибок 2

--- fit_life.py
+++ fit_life.py
@@ -8,27 +8,31 @@ print("=" * 60)
 # 1. Знакомство
 user_name = input('Как вас зовут? ')
 
+
 # 2. Сбор данных
 # Функция для проверки числа, с передачей аргументов
 # 1 аргумент - подсказка, 2 аргумент - тип данных
 # Основу взял у ии практикума, дополнил на основе гайдов из интернета
 def check_number(hint, number_type):
+    """Вычисляет тип ввода пользователя"""
     while True:
+
         try:
             number = number_type(input(hint))
             if number <= 0:
                 print('Значение должно быть больше 0')
                 continue
             else:
-                print("=" * 60 ) 
+                print('=' * 60)
                 return number
         except ValueError:
             print('Пожалуйста, введите обычное число!')
 
-# объявление переменных для расчетов с вызовом функции 
-user_age = check_number("Введите ваш возраст (например 45): ", int)
-user_weight = check_number('Укажите ваш вес в килограммах (например, 40.5 или 40):', float)
-user_height = check_number('Укажите ваш рост в метрах (например: 1.65 или 2): ', float)
+
+# объявление переменных для расчетов с вызовом функции
+user_age = check_number('Введите ваш возраст (пр.: 45): ', int)
+user_weight = check_number('Ваш вес в КГ (пр.: 40.5 или 40): ', float)
+user_height = check_number('Ваш рост в метрах (пр.: 1.65 / 2): ', float)
 
 # 3. Логика расчетов
 # Индекс массы тела
@@ -37,5 +41,8 @@ bmi = user_weight / (user_height**2)
 water_needed = (user_weight * WATER_PER_KG) / WATER_ON_LITTER
 
 # 4. Вывод результата
-print(f'Привет, {user_name}! \nВаш возраст {user_age} (лет/года) \nИМТ: {round(bmi, 2)} \nРекомендуемая норма воды: {round(water_needed, 1)} л. в день')
+print(f'Привет, {user_name}!')
+print(f'Ваш возраст {user_age} (лет/года)')
+print(f'ИМТ: {round(bmi, 2)}')
+print(f'Рекомендуемая норма воды: {round(water_needed, 1)} л. в день')
 print('Расчет окончен. Будьте здоровы!')
